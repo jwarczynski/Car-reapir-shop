aa23d5c0e44046598c65ec0bac877eb59c39c562
Marcin Szwarc <marcinszwarc@hotmail.com>
2022-12-20 17:49:56 +0100

    +Tabela partsToCarModels

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -13,8 +13,9 @@ namespace WarsztatSamochodowy.Services
     internal class DatabaseService : IDisposable
     {
         public const string TABLE_PARTS = "parts";
-        public const string TABLE_CAR_MODELS = "carmodels";
-        public const string TABLE_CAR_MANUFACTURERS = "carmanufacturers";
+        public const string TABLE_CAR_MODELS = "carModels";
+        public const string TABLE_CAR_MANUFACTURERS = "carManufacturers";
+        public const string TABLE_PARTS_CAR_MODELS = "partsToCarModels";
 
         private readonly MySqlConnection mySqlConnection;
         private const string connectionString = "server=localhost;user=root;database=warsztat;port=3306;password=password";
