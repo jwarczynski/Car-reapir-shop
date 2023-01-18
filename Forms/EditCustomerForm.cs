2646310fc44795bba531f228c8fed897e72a728a
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-01-18 18:51:44 +0100

    Walidacja danych wejściowych

--- Forms/EditCustomerForm.cs
+++ Forms/EditCustomerForm.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -51,6 +52,29 @@ namespace WarsztatSamochodowy.Forms
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(tbCustomerName.Text) || !Regex.IsMatch(tbCustomerName.Text, @"\w+ \w+"))
+            {
+                MessageBox.Show("Podaj imię i nazwisko klienta", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(tbPhoneNumber.Text) || !Regex.IsMatch(tbPhoneNumber.Text, @"\+?[-0-9 ]{5,15}"))
+            {
+                MessageBox.Show("Podaj numer telefonu klienta", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!string.IsNullOrWhiteSpace(tbEmailAddress.Text) && !Regex.IsMatch(tbEmailAddress.Text, @"[A-Za-z0-9\-.+_]+@[A-Za-z0-9\-.+_]+"))
+            {
+                MessageBox.Show("Podaj poprawny adres e-mail klienta", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (Regex.IsMatch(mtbTaxId.Text, @"\d") && !mtbTaxId.MaskCompleted)
+            {
+                MessageBox.Show("Podaj poprawny NIP klienta", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            string? emailAddress = string.IsNullOrWhiteSpace(tbEmailAddress.Text) ? null : tbEmailAddress.Text;
+            string? taxId = mtbTaxId.MaskCompleted ? mtbTaxId.Text : null;
+
             try
             {
                 var db = DatabaseService.Get();
@@ -61,8 +85,8 @@ namespace WarsztatSamochodowy.Forms
                         new() {
                             ["fullName"] = tbCustomerName.Text,
                             ["phoneNumber"] = tbPhoneNumber.Text,
-                            ["email"] = tbEmailAddress.Text,
-                            ["taxId"] = mtbTaxId.Text
+                            ["email"] = emailAddress,
+                            ["taxId"] = taxId
                         });
                 }
                 else
@@ -72,14 +96,14 @@ namespace WarsztatSamochodowy.Forms
                         {
                             ["fullName"] = tbCustomerName.Text,
                             ["phoneNumber"] = tbPhoneNumber.Text,
-                            ["email"] = tbEmailAddress.Text,
-                            ["taxId"] = mtbTaxId.Text
+                            ["email"] = emailAddress,
+                            ["taxId"] = taxId
                         });
                 }
                 this.Close();
             } catch(MySqlException ex)
             {
-                string message = "Nie udało się zapisać klienta do bazy." + ex.Message;
+                string message = "Nie udało się zapisać klienta do bazy.";
                 MessageBox.Show(message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
